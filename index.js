d4ccdf81cf489b4ad5ad24633a6d77b9ab82ce8e
chintan-albiorix <vsainagesh04@gmail.com>
2022-06-12 20:49:41 +0530

    backend updated

--- index.js
+++ index.js
@@ -2,21 +2,32 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const dbConnect = require("./config/db");
+const dotenv = require('dotenv');
+dotenv.config();
 
 dbConnect();
 
-
 app.use(express.json({ extended: false }));
 
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public", "index.html"))
+);
 
-app.use(express.static(__dirname + "/public"))
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')))
+// mount routes
+app.use("/api/user", require("./server/routes/api/user"));
+app.use("/api/posts", require("./server/routes/api/posts"));
+app.use("/api/auth", require("./server/routes/api/auth"));
+app.use("/api/profile", require("./server/routes/api/profile"));
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST, OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+  );
+  next();
 });
 
 const PORT = process.env.PORT || "3000";
