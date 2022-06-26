1e1cd65fda1e5f75d0b5c211269317a38d7c5157
chintan-albiorix <vsainagesh04@gmail.com>
2022-06-26 15:07:43 +0530

    frontend code updated

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const app = express();
 const path = require("path");
 const dbConnect = require("./config/db");
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 dbConnect();
 
 app.use(express.json({ extended: false }));
 
-app.use(express.static(__dirname + "/public"));
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public", "index.html"))
+app.use(express.static("client/public"));
+
+
+app.get("/", (req, res) =>{
+  res.sendFile(path.join(__dirname,'client', "public", "index.html"))
+}
 );
 
 // mount routes
@@ -19,6 +23,7 @@ app.use("/api/user", require("./server/routes/api/user"));
 app.use("/api/posts", require("./server/routes/api/posts"));
 app.use("/api/auth", require("./server/routes/api/auth"));
 app.use("/api/profile", require("./server/routes/api/profile"));
+
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -30,5 +35,5 @@ app.use(function (req, res, next) {
   next();
 });
 
-const PORT = process.env.PORT || "3000";
+const PORT = process.env.PORT || "5000";
 app.listen(PORT, () => console.log(`server started on port no ${PORT}`));
