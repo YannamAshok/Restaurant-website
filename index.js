5f43253acdfcfa03716e78fda94adc740a475db9
chintan-albiorix <vsainagesh04@gmail.com>
2022-07-07 22:59:33 +0530

    express updated

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
 const dbConnect = require("./config/db");
-const dotenv = require('dotenv');
 
-dotenv.config();
 
 dbConnect();
 
-app.use(express.json({ extended: false }));
+app.use(express.json());
 
 app.use(express.static("client/public"));
 app.get("/", (req, res) =>{
@@ -33,5 +32,5 @@ app.use(function (req, res, next) {
   next();
 });
 
-const PORT = process.env.PORT || "5000";
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server started on port no ${PORT}`));
